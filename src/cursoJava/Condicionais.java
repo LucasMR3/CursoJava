8336cb7b6d9d4bce84541f6f148b37fc797e527a
LucasMR-SM <lucas.ribeiro@sysmap.com.br>
2019-10-11 16:55:51 -0300

    condicionais

--- src/cursoJava/Condicionais.java
+++ src/cursoJava/Condicionais.java
@@ -1,21 +1,57 @@
 package cursoJava;
+
 import java.util.Scanner;
 
 public class Condicionais {
 
 	private static Scanner sc;
-	
+
 	public static void main(String[] args) {
 
 		sc = new Scanner(System.in);
 
-		int horario;
+		System.out.println("Horario em else-if");
+		int horario = sc.nextInt();
+
+		if (horario > 5 && horario <= 12) {
+			System.out.println("Bom dia");
+		} else if (horario > 12 && horario < 18) {
+			System.out.println("Boa tarde");
+		} else if (horario > 18 && horario < 24) {
+			System.out.println("Boa noite");
+		} else if (horario >= 0 && horario <= 5) {
+			System.out.println("Boa madrugada");
+		} else {
+			System.out.println("Horario não reconhecido");
+		}
+
+		System.out.println("Dias Switch Case");
+		int dias = sc.nextInt();
+
+		switch (dias) {
+		case 1:
+			System.out.println("Domingo");
+			break;
+
+		case 2:
+			System.out.println("Segunda");
+			break;
+
+		case 3:
+			System.out.println("Terça");
+			break;
+
+		default:
+			System.out.println("Dia não reconhecido");
+			break;
+		}
 		
-		horario = sc.nextInt();
+		int preco = sc.nextInt();
 		
 		
+		System.out.println((preco == 1) ? "Verdadeiro" : "False");
 		
-		System.out.println(horario);
+		// true:false
 		
 		sc.close();
 	}
