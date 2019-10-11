ea39c15a07454f6252175e1816996fe644377892
LucasMR-SM <lucas.ribeiro@sysmap.com.br>
2019-10-11 13:20:07 -0300

    correcao Condicionais

---
+++ src/cursoJava/Condicionais.java
@@ -0,0 +1,22 @@
+package cursoJava;
+import java.util.Scanner;
+
+public class Condicionais {
+
+	private static Scanner sc;
+	
+	public static void main(String[] args) {
+
+		sc = new Scanner(System.in);
+
+		int horario;
+		
+		horario = sc.nextInt();
+		
+		
+		
+		System.out.println(horario);
+		
+		sc.close();
+	}
+}
