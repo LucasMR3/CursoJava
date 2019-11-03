9e120ec8f7d11d20490c4229eed379ac8cc94f6e
LucasMR-SM <lucas.ribeiro@sysmap.com.br>
2019-11-03 14:15:04 -0300

    For use and example

---
+++ src/cursoJava/JavaFor.java
@@ -0,0 +1,39 @@
+package cursoJava;
+
+import java.util.Scanner;
+
+public class JavaFor {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int x, aux;
+		
+		
+		System.out.printf("Digite o valor a ser somado 5x em While: ");
+		x = sc.nextInt();
+		
+		aux = 0;
+		while (aux < 5) {
+		x++;
+		System.out.printf("O valor eh: %d \n", x);
+		aux++;
+		}
+
+		
+		
+		System.out.printf("\nDigite o valor a ser somado 5x em FOR: ");
+		x = sc.nextInt();
+
+		for (aux = 0; aux < 5; aux++) {
+			x++;
+			System.out.printf("O valor eh: %d\n", x);
+		}
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
