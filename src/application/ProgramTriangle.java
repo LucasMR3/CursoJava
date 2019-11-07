4dcf93c65524d268f306a0e88fc2dc633d80ab78
LucasMR3 <lucas_mr@live.com>
2019-11-06 23:24:19 -0300

    update Triangle and Products

--- src/application/ProgramTriangle.java
+++ src/application/ProgramTriangle.java
@@ -5,44 +5,38 @@ import java.util.Scanner;
 
 import entities.Triangle;
 
-public class Program {
-
+public class ProgramTriangle {
 	public static void main(String[] args) {
-
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
 //		double xA, xB, xC, yA, yB, yC;  // variavel stack
-		
 		Triangle x, y;
-		x = new Triangle(); // variavel heap, ponteiro
+		x = new Triangle(); // variavel heap = ponteiro
 		y = new Triangle();
-		
-		
-		
+
 		System.out.println("Enter the measures of triangle X: ");
 		x.a = sc.nextDouble();
 		x.b = sc.nextDouble();
 		x.c = sc.nextDouble();
-		
+
 		System.out.println("Enter the measures of triangle Y: ");
 		y.a = sc.nextDouble();
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
-		
+
 		double areaX = x.area();
 		double areaY = y.area();
-		
+
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
-		
+
 		if (areaX > areaY) {
 			System.out.println("Larger area: X");
 		} else {
 			System.out.println("Larger area: Y");
 		}
-		
+
 		sc.close();
 	}
-
 }
--- src/entities/Triangle.java
+++ src/entities/Triangle.java
@@ -1,12 +1,16 @@
 package entities;
 
 public class Triangle {
-	public double a,b, c;
-	
-	public double area(){
-		double p = (a+b+c)/2.0;
-		return Math.sqrt(p*(p-a)*(p-b)*(p-c));
-		
+	public double a, b, c;
+
+	public double area() {
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
 	}
-	
 }
+
+/*************************
+ * Reaproveitamento de código: Eliminamos código repetido do programa
+ * Delegação de Responsabilidades: O próprio triângulo é responsável pelo seu
+ * cálculo
+ ************************/
