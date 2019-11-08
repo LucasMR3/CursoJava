ffdee7d4b635bc8f2ef19fdab67ac25e0fb85a58
LucasMR3 <lucas_mr@live.com>
2019-11-08 10:19:38 -0300

    updates

--- src/application/ProgramProducts.java
+++ src/application/ProgramProducts.java
@@ -10,21 +10,30 @@ public class ProgramProducts {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		Product product = new Product();
+		Product product = new Product(); // object
 		System.out.println("Enter product data: ");
-		
-		System.out.println("Name: ");
+
+		System.out.print("Name: ");
 		product.name = sc.nextLine();
-		
+
 		System.out.print("Price: ");
 		product.price = sc.nextDouble();
-		
+
 		System.out.print("Quantity in stock: ");
 		product.quantity = sc.nextInt();
+
+		System.out.printf("\n\nProduct data: " + product);
 		
-		//test-hotfix
+		System.out.printf("\n\nEnter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		System.out.printf("\n\nUpdated data: " + product);
 		
-		System.out.println(product);
+		System.out.printf("\n\nEnter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		System.out.printf("\n\nUpdated data: " + product);
+		
 		
 		
 		sc.close();
