87cca9941577edd2ee018cfa0b268cc2730e536c
LucasMR3 <lucas_mr@live.com>
2020-11-16 12:49:22 -0300

    Evaluator added

--- src/UNASP/CalculoNumerico/Bisection.java
+++ src/UNASP/CalculoNumerico/Bisection.java
@@ -2,17 +2,22 @@ package UNASP.CalculoNumerico;
 
 public class Bisection {
     public static void main(String[] args) {
-        double rootA = 0, rootB = 1, result, variation = 0.01;
+        double rootA = 0, rootB = 1, variation = 0.01;
 
-        result = bisectionMethod(rootA, rootB, variation);
-
-        System.out.println("\nRaiz aproximada = " + String.format("%.3f", result));
+        bisectionMethod(rootA, rootB, variation);
     }
 
-    private static double bisectionMethod(double rootA, double rootB, double variation) {
-        double bisection, functionA, bisectionSolved;
+    private static void bisectionMethod(double rootA, double rootB, double variation) {
+        double bisection, functionA, functionB, bisectionSolved;
 
         functionA = mathEquation(rootA);
+        functionB = mathEquation(rootB);
+
+        // Se o resultado de f(a) * f(b) > 0 não existem raizes reais pelo Toerema de Bolzano
+        if (functionA * functionB > 0) {
+            System.out.println("Não existem raizes reais para essa equação.");
+            return;
+        }
 
         for (int i = 1; Math.abs((rootB - rootA)) > variation; i++) {
             bisection = (rootA + rootB) / 2;
@@ -23,11 +28,11 @@ public class Bisection {
                 rootA = bisection;
                 functionA = bisectionSolved;
             }
-            System.out.println("\nInteração #" + i);
+            System.out.println("\nIteração #" + i);
             System.out.println("Raiz A = " + rootA + " | Root B = " + rootB);
             System.out.print("Resultado = " + (rootA + rootB) / 2 + "\n");
         }
-        return (rootA + rootB) / 2;
+        System.out.println("\nRaiz aproximada = " + String.format("%.3f", (rootA + rootB) / 2));
     }
 
     private static double mathEquation(double variable) {
